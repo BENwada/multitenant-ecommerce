2b34e7bdf5b009d0d93ef1723bb7d0b177a86303
BENwada <domo0103wada@gmail.com>
2025-05-25 21:37:39 +0900

    06: categories finalization

--- src/app/(app)/(home)/search-filters/subcategory-menu.tsx
+++ src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import { Category } from "@/payload-types";
+import { CustomCategory } from "../types";
 
 interface Props {
-  category: Category;
+  category: CustomCategory;
   isOpen: boolean;
   position: { top: number; left: number };
 }
@@ -32,7 +33,7 @@ export const SubcategoryMenu = ({ category, isOpen, position }: Props) => {
           {category.subcategories.map((subcategory: Category) => (
             <Link
               key={subcategory.slug}
-              href="/"
+              href={`/${category.slug}/${subcategory.slug}`}
               className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center justify-between underline font-medium"
             >
               {subcategory.name}
